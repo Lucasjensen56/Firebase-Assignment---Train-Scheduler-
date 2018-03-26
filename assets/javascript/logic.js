c3c463fa4557e84d8dc1e789d813edc421fa5449
Lucas Jensen <lucasjensen56@gmail.com>
2018-03-26 18:30:39 -0500

    scaffolded out main fields on webpage

---
+++ assets/javascript/logic.js
@@ -0,0 +1,14 @@
+
+
+// firebase informaiton
+
+var config = {
+    apiKey: "AIzaSyCZ252TUQewOyRPtRs5qY0Jw3ouZmVrsH0",
+    authDomain: "train-times-1aa7b.firebaseapp.com",
+    databaseURL: "https://train-times-1aa7b.firebaseio.com",
+    projectId: "train-times-1aa7b",
+    storageBucket: "",
+    messagingSenderId: "908116121506"
+  };
+  
+  firebase.initializeApp(config);
